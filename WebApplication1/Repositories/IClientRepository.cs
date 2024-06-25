319708de57d0e9cada2af055402e111ee229de64
s27973 <s27973@pjwstk.edu.pl>
2024-06-25 12:31:01 +0200

    commit2

---
+++ WebApplication1/Repositories/IClientRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Repositories;
+
+public interface IClientRepository
+{
+    Task<bool> DoesClientExist(int id);
+    Task<bool> DoesCarExist(int carId);
+    Task<ClientDto> GetClientWithRentals(int clientId);
+    Task AddNewClientWithRental(NewClientWithRentalDto newClientWithRental);
+}
